b4b9be1ed57330ceea94b0a8dbffc8937eead2b6
Brandon <brandoncyu@gmail.com>
2018-09-20 14:52:40 -0700

    added login and register forms

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+// import Register from './components/signin/Register'
+// import Login from './components/signin/Login'
+import Signin from './components/signin/Signin'
 import './App.css';
+import { Switch, Route, Redirect } from 'react-router-dom'
 
 class App extends Component {
+  constructor(){
+    super()
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Switch>
+
+        <Route exact path="/signin" render={()=>{
+          return <Signin />
+        }} />
+        <Redirect to="/signin" />
+      </Switch>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <App />
+    </Switch>
+  </Router>, document.getElementById('root'));
 registerServiceWorker();
