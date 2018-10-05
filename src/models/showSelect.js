c4d3ff7702435c4d4cee750dbd0315c595277163
Brandon <brandoncyu@gmail.com>
2018-10-05 13:23:53 -0700

    change tvmaze api

--- src/models/showSelect.js
+++ src/models/showSelect.js
@@ -1,22 +1,23 @@
 import axios from 'axios'
-const TV_URL = 'https://api.tvmaze.com'
+
+const TV_URL_SERVER = 'https://api.tvmaze.com'
 
 export const searchAll = async (title) => {
-  const response = await axios.get(`${TV_URL}/search/shows?q=${title}`)
+  const response = await axios.get(`${TV_URL_SERVER }/search/shows?q=${title}`)
   return response.data
 }
 
 export const searchOne = async (title) => {
-  const response = await axios.get(`${TV_URL}/singlesearch/shows?q=${title}`)
+  const response = await axios.get(`${TV_URL_SERVER}/singlesearch/shows?q=${title}`)
   return response.data
 }
 
 export const searchOneByNumber = async (id) => {
-  const response = await axios.get(`${TV_URL}/shows/${id}/episodes`)
+  const response = await axios.get(`${TV_URL_SERVER}/shows/${id}/episodes`)
   return response.data
 }
 
 export const searchOneWithEpisodes = async (title) => {
-  const response = await axios.get(`${TV_URL}/singlesearch/shows?q=${title}&embed=episodes`)
+  const response = await axios.get(`${TV_URL_SERVER}/singlesearch/shows?q=${title}&embed=episodes`)
   return response.data
 }
